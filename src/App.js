89d47faf0663ae01f8e711e395171e9405ea557d
root <root@Nour.>
2025-10-08 03:06:14 +0300

    Initial commit with Reactd Dockerfile

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-            Holaa from Nourhan !!
+            Hello from Nourhan !!
         </p>
         <a
           className="App-link"
